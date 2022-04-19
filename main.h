1efa7d8fa2b6a440ec2d6b7f4c21d64e9596514f
Latifah <kimtifah2@gmail.com>
2022-04-19 21:39:28 +0200

    Update main.h

--- main.h
+++ main.h
@@ -1,37 +1,28 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h> /* va_list */
-#include <stdlib.h> /* malloc, free */
-#include <unistd.h> /* write */
-
-/* helper functions */
-char* (*get_func(char i))(va_list);
-char *create_buffer(void);
-void write_buffer(char *buffer, int len, va_list list);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
-
-/* printf functions */
-int _printf(const char *format, ...);
-char *print_s(va_list list);
-char *print_c(va_list list);
-char *print_d(va_list list);
-char *itob(va_list list);
-char *rot13(va_list list);
-char *rev_string(va_list list);
-char *itoOctal(va_list list);
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
 
 /**
- * struct types - struct
- * @id: identifier of type to print (e.g. c means char)
- * @func: ptr to functions that print according to identifier (e.g. print_c)
+ * struct func_type - type structure
+ * @t: pointer to the argument
+ * @f: pointer-function associated with the argumen
  */
-
-typedef struct types
+typedef struct func_type
 {
-	char id;
-	char* (*func)(va_list);
-} print;
+	char *t;
+	int (*f)(va_list);
+} func_t;
 
-#endif
+int (*get_func(const char *format))(va_list);
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_str(va_list args);
+int print_char(va_list args);
+int print_pct(va_list args);
+
+#endif /* MAIN_H */
